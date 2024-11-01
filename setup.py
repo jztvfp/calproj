bd265b7ce3e1891b48b7bd4b3444b4b4ae777a24
Ian <ian.payne@genii-bi.com>
2024-11-01 13:41:41 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,37 @@
+"""
+setup.py configuration script describing how to build and package this project.
+
+This file is primarily used by the setuptools library and typically should not
+be executed directly. See README.md for how to deploy, test, and run
+the ian_cal project.
+"""
+from setuptools import setup, find_packages
+
+import sys
+sys.path.append('./src')
+
+import datetime
+import ian_cal
+
+setup(
+    name="ian_cal",
+    # We use timestamp as Local version identifier (https://peps.python.org/pep-0440/#local-version-identifiers.)
+    # to ensure that changes to wheel package are picked up when used on all-purpose clusters
+    version=ian_cal.__version__ + "+" + datetime.datetime.utcnow().strftime("%Y%m%d.%H%M%S"),
+    url="https://databricks.com",
+    author="jztvfp@hotmail.com",
+    description="wheel file based on ian_cal/src",
+    packages=find_packages(where='./src'),
+    package_dir={'': 'src'},
+    entry_points={
+        "packages": [
+            "main=ian_cal.main:main"
+        ]
+    },
+    install_requires=[
+        # Dependencies in case the output wheel file is used as a library dependency.
+        # For defining dependencies, when this package is used in Databricks, see:
+        # https://docs.databricks.com/dev-tools/bundles/library-dependencies.html
+        "setuptools"
+    ],
+)
